7bef3900605b957dd96d03b438ca731e8a6f9d0c
Michael Stallings <stall84@gmail.com>
2020-02-22 08:44:36 -0500

    first commit of exercise.. didn't really accomplish anything

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// javascript for dynamic-webpage
+
+
+
+var grabBody = document.getElementsByTagName('body');
+var divMaker = document.createElement('div');
+divMaker.id = 'header-sec';
+grabBody.appendChild(divMaker);
+
