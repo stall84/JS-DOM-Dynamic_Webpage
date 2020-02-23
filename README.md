dd7b4443d6d8867df63660262ef64d5b8569304f
Michael Stallings <stall84@gmail.com>
2020-02-22 23:48:37 -0500

    First full commit of setup to complete problem/exercise 2/22

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dynamic_Webpage Exercise
+
+-This exercise I will attempt to create a dynamic web page using javascript instead of directly manipulating html elements in the index.html document.
+
+-Will do this via DOM methods from the script.js file
--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 // javascript for dynamic-webpage
 
 
+var body = document.body
+var div1 = document.createElement('div');
+var element1 = document.createElement('h1');
+element1.innerHTML = "SUP BEETCHES!";
 
-var grabBody = document.getElementsByTagName('body');
-var divMaker = document.createElement('div');
-divMaker.id = 'header-sec';
-grabBody.appendChild(divMaker);
+body.appendChild(div1);
+div1.appendChild(element1);
+//var divMaker = document.createElement('div');
+//divMaker.id = 'header-sec';
+//grabBody.appendChild(divMaker);
 
