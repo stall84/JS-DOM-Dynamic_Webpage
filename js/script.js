c11bc7a303ba562938dc91f915435c9b242a5060
Michael Stallings <stall84@gmail.com>
2020-02-23 08:32:49 -0500

    worked through issues of container returning null until solved by moving script tag to bottom of page just before closing html tag. afterwhich js code began working on html page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // javascript for dynamic-webpage
 
-// create variable to grab container div in html body
+// create variable to grab container div (that's already present) in html body
 var container = document.getElementById('container');
 
+// create variables to store the header tag and h1 tag and nav tag you'll be creating
 
+var header = document.createElement('header');
+var title = document.createElement('h1');
+var nav = document.createElement('nav');
 
+// inject the header tag/section into the document's overall wrapper 'container' via appendChild method
+container.appendChild(header);
+// once our header is in place we can now start adding the h1 div and nav elements to the newly created header section
+// via same appendChild method
+header.appendChild(title);
+header.appendChild(nav);
+
+title.innerHTML = 'HighOnCoding';
+nav.innerHTML = '<ul><li>Home</li><li>Categories</li></ul>';
